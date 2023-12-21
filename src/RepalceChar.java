473e51a006f75443a07841c86a621f89d90dd6ae
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-21 17:16:40 +0530

    replace the letter e with letter i

--- src/RepalceChar.java
+++ src/RepalceChar.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class RepalceChar {
+//Input a string from the user. Create a new string called ‘result’ in which
+// you will replace the letter ‘e’ in the original string with letter ‘i’.
+import java.util.Scanner;
+public class RepalceChar {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter any string ");
+        String str=sc.nextLine();
+        //String str1=str.replace('i','e');
+        //System.out.println("String after replace the char i with e:"+" "+str1);
+        //other types
+        String result=" ";
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)=='e')
+            {
+                result +='i';
+            }
+            else
+            {
+                result +=str.charAt(i);
+            }
+        }
+        System.out.println("string after replace the char :"+" "+result);
+    }
 }
